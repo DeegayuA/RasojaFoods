7c2d4a65714c9b2bb6d5330981984d97cee26fc3
Deeghayu Adhikari <63475473+DeegayuA@users.noreply.github.com>
2023-02-19 21:10:45 +0530

    new

--- rasoja-foods/src/container/owner/owner.jsx
+++ rasoja-foods/src/container/owner/owner.jsx
@@ -16,7 +16,7 @@ const Owner = () => (
         <h1 className="headtext__cormorant">What We Believe In</h1>
         <div className="app__owner-content">
           <div className="app__owner-content_quote">
-            <img src={images.quote} alt="quote" />
+           
             <p className="p__opensans">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos molestias, laborum id voluptatum quis hic aperiam explicabo placeat inventore illo labore ad ea unde.</p>
           </div>
           <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, suscipit.</p>
